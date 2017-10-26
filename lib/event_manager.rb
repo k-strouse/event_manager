657ecb70e4ea539c0b4e387e88ada65ba6e2d5b0
Kyle Strouse <kstrouse09@gmail.com>
2017-10-26 12:36:18 -0500

    Add some comments

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,1 +1,12 @@
 puts "EventManager Initialized!"
+
+#contents = File.read "event_attendees.csv"
+#puts contents
+
+lines = File.readlines "event_attendees.csv"		#read the file and convert into an array of lines
+lines.each_with_index { |line, index| 
+	next if index == 0										#skip the first line of the array (which has the header row)
+	columns = line.split(",")							#split the rows of data into columns and put into columns array(each array index is a column)
+	name = columns[2]											#access the 'fist_Name' attribute in our array of columns
+	puts name
+}
